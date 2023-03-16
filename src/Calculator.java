61b5483c0b62ef056c682d1f3f509b9a657d0b84
satria wibowo <extencezone02@gmail.com>
2023-03-16 14:01:10 +0700

    add java

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*; //FOR resolve symbol 'ActionListener'
+
+public class Calculator implements ActionListener{
+
+//    DECLARE J-FRAME
+    JFrame frame;
+    JTextField textField;
+    Calculator() {
+
+    }
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //TODO Auto-generated method stub
+
+    }
+}
